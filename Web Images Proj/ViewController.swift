88bdf2ed135b7069841d04ff97fa06d126d455f9
curriedav <curriedav@gmail.com>
2014-12-02 14:24:08 -0800

    Initial Commit

---
+++ Web Images Proj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Images Proj
+//
+//  Created by David Currie on 12/2/14.
+//  Copyright (c) 2014 Urban Airship. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
